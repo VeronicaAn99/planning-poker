7e9dfb3c76b4fd01ca67cedfb5d5444c1a7d4349
nicaan <nica.an23@gmail.com>
2025-09-16 23:44:39 +0300

    Fix Tailwind CSSintegration; remove PostCSS and Tailwind configuration files, and update Card component styles

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,3 +1,7 @@
 export const Card = () => {
-	return <div>Card</div>;
+	return (
+		<div className="w-40 h-40 bg-[#f334f3] rounded-lg border border-gray-400 flex items-center justify-center">
+			Card
+		</div>
+	);
 };
