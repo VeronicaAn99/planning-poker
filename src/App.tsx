66c30f6a2c0e9cd0d34c5045c498075ec736a343
nicaan <nica.an23@gmail.com>
2025-09-16 23:28:06 +0300

    Update index.html and App.tsx: Change document type to HTML5, update title to 'Planning Poker', and render Home component in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import Home from "./pages/Home";
+
 function App() {
-	return <>Hello World</>;
+	return <Home />;
 }
 
 export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home: React.FC = () => {
+	return (
+		<div className="min-h-screen flex flex-col items-center justify-center p-6 gap-6">
+			<h1 className="text-2xl font-bold">Planning Poker</h1>
+		</div>
+	);
+};
+
+export default Home;
