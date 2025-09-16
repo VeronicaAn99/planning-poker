b7160f33fd15d83805e292f92e5a13117f3e69aa
nicaan <nica.an23@gmail.com>
2025-09-16 23:29:35 +0300

    Add Card component and integrate it into Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { Card } from "../components/Card";
 
 const Home: React.FC = () => {
 	return (
 		<div className="min-h-screen flex flex-col items-center justify-center p-6 gap-6">
 			<h1 className="text-2xl font-bold">Planning Poker</h1>
+
+			{Array.from({ length: 10 }).map((_, index) => (
+				<Card key={index} />
+			))}
 		</div>
 	);
 };
